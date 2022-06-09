1fa61876ea025d389473cd2302cfb2e0310a3850
vinodjupalli <vinodhari7jupalli@gmail.com>
2022-06-09 09:22:16 +0530

    Added project

---
+++ src/main/java/com/vinod/MavenExample1Application.java
@@ -0,0 +1,13 @@
+package com.vinod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenExample1Application.class, args);
+	}
+
+}
